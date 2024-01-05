abf15fec739dc16216c054c2fa81617eae54d2d2
kwoo2 <kengthong.woo@illumina.com>
2024-01-05 18:03:10 +0800

    created room and customer

---
+++ hotel/src/main/java/org/example/models/Customer.java
@@ -0,0 +1,12 @@
+package org.example.models;
+
+public class Customer {
+    private String name;
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    private String getName() {
+        return name;
+    }
+}
---
+++ hotel/src/main/java/org/example/models/Room.java
@@ -0,0 +1,34 @@
+package org.example.models;
+
+public class Room implements Comparable<Room> {
+    public enum Status {
+        AVAILABLE, OCCUPIED, VACANT, REPAIR
+    }
+
+    private String name;
+    private Status status;
+    private int index;
+    private Customer currCustomer;
+
+    public Room(String name, int idx) {
+        this.name = name;
+        this.index = idx;
+    }
+
+    public Status getStatus() {
+        return this.status;
+    }
+
+    public int getIndex() { return this.index; }
+
+    public void setOccupied(Customer customer) {
+        this.status = Status.OCCUPIED;
+        this.currCustomer = customer;
+    }
+
+    @Override
+    public int compareTo(Room other) {
+        // Comparing based on priority (ascending order)
+        return Integer.compare(this.index, other.getIndex());
+    }
+}
