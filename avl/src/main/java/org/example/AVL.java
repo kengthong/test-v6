75f70fe5c88c00136a7d1e5ff7b3822416c6c787
kwoo2 <kengthong.woo@illumina.com>
2024-01-08 21:30:45 +0800

    comment: added comments

--- avl/src/main/java/org/example/AVL.java
+++ avl/src/main/java/org/example/AVL.java
@@ -7,15 +7,60 @@ public class AVL<T extends Comparable<T>> {
     // Root node of AVL Tree
     public Node root;
 
+    // Additional properties: size, height
+
+    /**
+     * Public facing contains method
+     * @param value
+     * @return
+     */
     public boolean contains(T value) {
-
+        return contains(value, root);
     }
 
+    /**
+     * Recursively calls the private contains method onto its current, left or right nodes depending on the compare value
+     * @param value
+     * @param node
+     * @return
+     */
+    private boolean contains(T value, Node node) {
+    }
+
+    /**
+     * Public facing insert method
+     * @param value
+     * @return
+     */
     public boolean insert(T value) {
+        return insert(value, root);
+    }
+
+    /**
+     * Recursively calls the private insert method to insert the node into the left or right of the node, depending on compare value
+     * @param value
+     * @return
+     */
+    private boolean insert(T value, Node node) {
 
     }
 
+    /**
+     * Public facing remove method
+     * @param value
+     * @return
+     */
     public boolean remove(T value) {
+        return remove(value, root);
+    }
+
+    /**
+     * Recursively calls the private remove method on the current, left or right of node depending on compare value
+     * @param value
+     * @param node
+     * @return
+     */
+    public boolean remove(T value, Node node) {
 
     }
 
